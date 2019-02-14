b31f78e977ca2bf1161807e8546dc1fae91bc942
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-02-14 15:05:36 -0600

    First Assignment Created

---
+++ Interactive Scene/cs30-p5js-template-master/sketch.js
@@ -0,0 +1,16 @@
+// Interactive Scene
+// Sebastian Favel-Delorme
+// Feb 14, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  ellipse(mouseX, mouseY, 50, 50);
+}
