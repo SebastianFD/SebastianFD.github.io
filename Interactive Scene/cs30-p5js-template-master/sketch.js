d62397d1c24140d08e67e5ada62bb8c9fbf827b4
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-03-01 15:20:03 -0600

    Delay sorta stuff
    it works sorta.

--- Interactive Scene/cs30-p5js-template-master/sketch.js
+++ Interactive Scene/cs30-p5js-template-master/sketch.js
@@ -5,10 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let i = 0
+
+let i = 0; // <= *this is for the 5 circle placer*
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
 
 // *This is a random, 5 Circle placer*
 //while (i < 5) {
@@ -16,9 +18,28 @@ function setup() {
   //ellipse(random(width), random(height), 100, 100);
   //i++;
   //}
+
+
 }
 
 function draw() {
   background(220);
-  ellipse(mouseX, mouseY, 50, 50);
+  
+  //ellipse(mouseX, mouseY, 50, 50);
+
+
+  let delay = 0.05;
+  let x = 1;
+  let y = 1;
+  
+  let targetX = mouseX;
+  let directionX = targetX - x;
+  x = x + directionX * delay;
+
+  let targetY = mouseY;
+  let directionY = targetY - y;
+  y = y + directionY * delay;
+   
+ellipse(x, y, 50, 50);
+
 }
