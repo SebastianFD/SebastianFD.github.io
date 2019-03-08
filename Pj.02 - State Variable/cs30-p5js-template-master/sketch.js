c99e41470a8d95cdd528dae23d4f62a09791ba70
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-03-08 15:21:36 -0600

    State Variable Start Project
    Started working on state variable prjoect: "The Blob"

--- Pj.02 - State Variable/cs30-p5js-template-master/sketch.js
+++ Pj.02 - State Variable/cs30-p5js-template-master/sketch.js
@@ -5,11 +5,13 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let blobBeginningSize;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+  blobBeginningSize = 50
+  rect(0, windowHeight - blobBeginningSize, blobBeginningSize, blobBeginningSize)
 }
