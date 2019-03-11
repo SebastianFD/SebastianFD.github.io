b80af99b6436b2ee7df7bfcbafa33f294f9ec04f
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-03-10 18:02:11 -0600

    Added State Picker
    Added a state picker but broke the square, fix state picker

--- Pj.02 - State Variable/cs30-p5js-template-master/sketch.js
+++ Pj.02 - State Variable/cs30-p5js-template-master/sketch.js
@@ -5,13 +5,44 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let state;
 let blobBeginningSize;
+let blobPositionX;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  blobBeginningSize = windowWidth / 20;
+  blobPositionX = 0;
+  state = blobState1;
+  
 }
 
 function draw() {
   background(220);
-  blobBeginningSize = 50
-  rect(0, windowHeight - blobBeginningSize, blobBeginningSize, blobBeginningSize)
+  statePicker();
+  rect(blobPositionX, windowHeight - blobBeginningSize, blobWidth, blobBeginningSize);
+}
+
+
+function keyTyped() {
+  if (blobPositionX >= 0 && blobPositionX <= windowWidth - blobBeginningSize) {
+    if (key === 'd') {
+      blobPositionX = blobPositionX + blobBeginningSize;
+    }
+    else if (key === 'a') {
+      blobPositionX = blobPositionX - blobBeginningSize;
+    }
+    else if (key ==='f') {
+      state = 2;
+    }
+  }
+}
+
+function statePicker () {
+  if (state === 1){
+    blobWidth = blobBeginningSize
+  }
+  else if (state === 2) {
+    blobWidth = blobBeginningSize + blobBeginningSize;
+  }
 }
