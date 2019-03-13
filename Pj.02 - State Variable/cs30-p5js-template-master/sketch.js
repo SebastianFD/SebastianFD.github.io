5126c91049ed7e4912b7f03d0afb81507d6e4a19
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-03-13 15:21:33 -0600

    Worked on logic of statePicker
    worked on states and statePicker

--- Pj.02 - State Variable/cs30-p5js-template-master/sketch.js
+++ Pj.02 - State Variable/cs30-p5js-template-master/sketch.js
@@ -1,28 +1,30 @@
-// Project Title
-// Your Name
+// The Blob
+// Sebastian Favel-Delorme
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 let state;
-let blobBeginningSize;
-let blobPositionX;
+let blobBeginningSize; // This is the beginning size that the square would be
+let blobPositionX, blobPositionY; // This dictates where "the blob" will begin and fit around its size
+let blobWidth, blobHeight; // these are how big "the blob" actually is (located in the state picker)
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   blobBeginningSize = windowWidth / 20;
+  blobPositionY = windowHeight - blobBeginningSize
   blobPositionX = 0;
-  state = blobState1;
-  
+  state = 1;
+  blobHeight = blobBeginningSize;
+  blobWidth = blobBeginningSize;
+  statePicker();
 }
 
 function draw() {
   background(220);
-  statePicker();
-  rect(blobPositionX, windowHeight - blobBeginningSize, blobWidth, blobBeginningSize);
+  rect(blobPositionX, blobPositionY, blobWidth, blobHeight);
 }
-
 
 function keyTyped() {
   if (blobPositionX >= 0 && blobPositionX <= windowWidth - blobBeginningSize) {
@@ -33,16 +35,34 @@ function keyTyped() {
       blobPositionX = blobPositionX - blobBeginningSize;
     }
     else if (key ==='f') {
-      state = 2;
+      state = state + 1;
+      statePicker();
     }
   }
 }
 
 function statePicker () {
-  if (state === 1){
-    blobWidth = blobBeginningSize
+  if (state === 1) {
+    blobWidth = blobBeginningSize;
+    blobHeight = blobBeginningSize;
+    blobPositionX = 0;
+    blobPositionY = windowHeight - blobBeginningSize;
   }
   else if (state === 2) {
-    blobWidth = blobBeginningSize + blobBeginningSize;
+    blobWidth = blobWidth + blobBeginningSize;
+  }
+  else if (state === 3) {
+    blobHeight = blobHeight + blobBeginningSize;
+    blobPositionY = windowHeight - (2*blobBeginningSize); // need to add this because Y position is in top left corner
+  }
+  else if (state === 4) {
+    blobWidth = blobWidth + (2*blobBeginningSize);
+  }
+  else if (state === 5) {
+    blobHeight = blobHeight + (2*blobBeginningSize);
+    blobPositionY =  windowHeight - (4*blobBeginningSize);
+  }
+  else if (state === 6) {
+    state = 1;
   }
 }
