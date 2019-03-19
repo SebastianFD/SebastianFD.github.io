17c9b4f4880cc3814bda6f7dddbbbe5f3aa93294
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-03-19 15:20:44 -0600

    Descriptions for The Blob
    Just cleaned up and made descriptions for The Blob project

--- Pj.02 - State Variable/cs30-p5js-template-master/sketch.js
+++ Pj.02 - State Variable/cs30-p5js-template-master/sketch.js
@@ -5,25 +5,26 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let state;
+let stateTheBlob;
 let blobBeginningSize; // This is the beginning size that the square would be
 let blobPositionX, blobPositionY; // This dictates where "the blob" will begin and fit around its size
 let blobWidth, blobHeight; // these are how big "the blob" actually is (located in the state picker)
-
+let backgroundStage;
+let backgroundRed, backgroundGreen, backgroundBlue; // The Currect background changer
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   blobBeginningSize = windowWidth / 20;
   blobPositionY = windowHeight - blobBeginningSize
   blobPositionX = 0;
-  state = 1;
+  stateTheBlob = 1;
   blobHeight = blobBeginningSize;
   blobWidth = blobBeginningSize;
-  statePicker();
+  stateBackground = 1
 }
 
 function draw() {
-  background(220);
+  background(backgroundRed, backgroundGreen, backgroundBlue);
   rect(blobPositionX, blobPositionY, blobWidth, blobHeight);
 }
 
@@ -36,34 +37,49 @@ function keyTyped() {
       blobPositionX = blobPositionX - blobBeginningSize;
     }
     else if (key ==='f') {
-      state = state + 1;
-      statePicker();
+      stateTheBlob = stateTheBlob + 1;
+      statePickerBlob();
     }
   }
 }
 
-function statePicker () {
-  if (state === 1) {
+function statePickerBlob() {
+  if (stateTheBlob === 1) { // The beginning size + base size of The Blob
     blobWidth = blobBeginningSize;
     blobHeight = blobBeginningSize;
     blobPositionX = blobPositionX;
     blobPositionY = windowHeight - blobBeginningSize;
   }
-  else if (state === 2) {
+  else if (stateTheBlob === 2) { // The blob grows to the right
     blobWidth = blobWidth + blobBeginningSize;
   }
-  else if (state === 3) {
+  else if (stateTheBlob === 3) { // The blob grows upward
     blobHeight = blobHeight + blobBeginningSize;
     blobPositionY = windowHeight - (2*blobBeginningSize); // need to add this because Y position is in top left corner
   }
-  else if (state === 4) {
+  else if (stateTheBlob === 4) { // Grows to the right again
     blobWidth = blobWidth + (2*blobBeginningSize);
   }
-  else if (state === 5) {
+  else if (stateTheBlob === 5) { // Grows upward again
     blobHeight = blobHeight + (2*blobBeginningSize);
     blobPositionY =  windowHeight - (4*blobBeginningSize);
   }
-  else if (state === 6) {
-    state = 0
+  else if (stateTheBlob === 6) { // Sets The blob at basically state 1 again
+    stateTheBlob = 0;
+    stateBackground = stateBackground + 1;
+    
   }
 }
+
+function statePickerBackground() {
+  if (stateBackground === 1) {
+    backgroundblue = 255;
+  }
+  else if (stateBackground === 2) {
+    backgroundGreen = 255;
+  }
+  else if (stateBackground === 3) {
+    backgroundRed = 255;
+  }
+}
+// 128 64 32 16 8 4 2 1
