6e6774782e6819b53d29811904cb33b38bcb574a
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-03-27 15:20:39 -0600

    Perlin Noise Demo
    Look at teacher demo to get it to work

--- Ex.05 Perlin Noise/cs30-p5js-template-master/sketch.js
+++ Ex.05 Perlin Noise/cs30-p5js-template-master/sketch.js
@@ -1,15 +1,37 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Perlin Noise Demo
 
+let time;
+let rectWidth;
+let myRectangle;
+let rects = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let numberOfRects = width;
+
+  time = 0;
+  rectWidth = width / numberOfRects;
+  generateInitialTerrain();
+
 }
 
 function draw() {
   background(220);
+  fill(0);
+  for(let i = 0; i < numberOfRects; i ++) {
+    rect(rects[i].x, rects[i].y, rects[i].width, rects[i].height);
+  }
+}
+
+function generateInitialTerrain() {
+  for (let i = 0; i < numberOfRects; i ++)
+    let rectHeight = noise(time) * height;
+    let myRectangle = {
+      height: rectHeight,
+      width: rectWidth,
+      x: i * rectWidth,
+      y: height - rectHeight,
+    };
+    rects.push(myRectangle);
+    time += 0.01;
 }
