f3ee7da4237a9d367a12d9dec1581f8de586a3b9
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-03-28 15:20:42 -0600

    Made new Example + fixed title
    Made Ex.06 and renamed Ex.05 (added a - )

---
+++ Ex.06 - Array of bouncing balls/cs30-p5js-template-master/sketch.js
@@ -0,0 +1,34 @@
+// Array of bouncing balls
+
+let ballArray = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  for(let i = 0; i < 100; i++) {
+    let newBall = {
+      x: random(width),
+      y: random(height),
+      dx: random(-5, 5),
+      dy: random(-5, 5),
+      radius: random(10, 50),
+      color: color(random(255), random(255), random(255), random(255)),
+    };
+  }
+  ballArray.push(newBall);
+}
+
+function draw() {
+  background(220);
+
+  for(let i = 0; i < ballArray.length; i++) {
+    //move ball
+    ballArray[i].x += ballArray[i].dx;
+    ballArray[i].y += ballArray[i].dy;
+
+    //display ball
+    fill(ballArray[i].color);
+    noStroke();
+    ellipse(ballArray[i].x, ballArray[i].y, ballArray[i].radius*2, ballArray[i].radius*2);
+  }
+}
