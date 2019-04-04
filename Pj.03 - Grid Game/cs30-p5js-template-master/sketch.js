bff7e06895677ebf9d10fc96c07414dc5dd9ae6b
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-04-04 15:19:59 -0600

    Tests

--- Pj.03 - Grid Game/cs30-p5js-template-master/sketch.js
+++ Pj.03 - Grid Game/cs30-p5js-template-master/sketch.js
@@ -5,11 +5,38 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let gridSize = 50;
+let grid;
+let cellSize;
+let autoPlay;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  
+  grid = createRandom2DArray(gridSize, gridSize);
+  cellSize = width/gridSize;
 }
 
 function draw() {
-  background(220);
+  background(255);
+  displayGrid();
+}
+
+function displayGrid() {
+  for (let y = 0; y < gridSize; y++) {
+    for (let x = 0; x < gridSize; x++) {
+      if (grid[y][x] === 0) {
+        fill(255);
+      }
+      else {
+        fill(0);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
 }
