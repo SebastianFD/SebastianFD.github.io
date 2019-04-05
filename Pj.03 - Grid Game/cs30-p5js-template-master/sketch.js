36bc9f7a596a5ef3ba1978864cf0a82b61302612
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-04-05 15:19:55 -0600

    Pj.03 work

--- Pj.03 - Grid Game/cs30-p5js-template-master/sketch.js
+++ Pj.03 - Grid Game/cs30-p5js-template-master/sketch.js
@@ -40,3 +40,28 @@ function displayGrid() {
     }
   }
 }
+
+
+function create2DArray(cols, rows) {
+  let emptyArray = [];
+  for (let i = 0; i < rows; i++) {
+    emptyArray.push([]);
+    for (let j = 0; j < cols; j++) {
+      emptyArray[i].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function createRandom2DArray(cols, rows) {
+  let emptyArray = [];
+  for (let i = 0; i < rows; i++) {
+    emptyArray.push([]);
+    for (let j = 0; j < cols; j++) {
+        emptyArray[i].push(0);
+      }
+    }
+  }
+  return emptyArray;
+}
+
