e2484d7b9362e689c45c32430771c363b6287ecb
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-04-10 15:19:29 -0600

    Pj.03 Update
    Zombie made and Bob (Human) made

--- Pj.03 - Grid Game/cs30-p5js-template-master/sketch.js
+++ Pj.03 - Grid Game/cs30-p5js-template-master/sketch.js
@@ -5,34 +5,41 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// Variables //
 let grid;
 let gridSize = 25;
 let cellSize;
-let centerScreen = round(gridSize / 2);
+let centerScreen;
+let zombieX, zombieY;
+let humanBobX, humanBobY;
+
 
 function setup() {
-
 if (windowHeight > windowWidth) {
   createCanvas(windowWidth, windowWidth)
 }
 else {
   createCanvas(windowHeight, windowHeight)
 }
-
 grid = createRandom2DArray(gridSize, gridSize);
 cellSize = width/gridSize;
+zombieX = 1;
+zombieY = 1;
+centerScreen = round(gridSize / 2);
 }
 
 function draw() {
   background(0);
   displayGrid();
+  humanBob();
+  zombie();
 }
 
 function displayGrid() {
   for (let y = 0; y < gridSize; y++) {
     for (let x = 0; x < gridSize; x++) {
       if (grid[y][x] === 0) {
-        fill(255);
+        fill(150);
       }
       else {
         fill(0);
@@ -58,3 +65,33 @@ function createRandom2DArray(cols, rows) {
   return emptyArray;
 }
 
+function zombie() {
+  fill(0);
+  rect(zombieX*cellSize, zombieY*cellSize, cellSize, cellSize);
+  if (gridSize === gridSize + 1) {
+
+  }
+}
+
+function keyTyped() {
+  if (key === 'd') {
+    zombieX = zombieX + 1
+  }
+  else if (key === 'a') {
+    zombieX = zombieX - 1
+  }
+  else if (key === 's') {
+    zombieY = zombieY + 1
+  }
+  else if (key === 'w') {
+    zombieY = zombieY - 1
+  }
+}
+
+function humanBob() {
+  fill(255);
+  rect(centerScreen*cellSize, centerScreen*cellSize, cellSize, cellSize);
+  if (zombieX === humanBobX && zombieY === humanBobY) {
+
+  }
+}
