63a56910849e5b4d6e1afe011d22986c21fd5117
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-05-23 15:21:11 -0600

    Added Ex.12
    Sorting Ex. (Bubble + Selective) Fix class Ex

---
+++ Ex.12 - Bubble Sort/cs30-p5js-template-master/sketch.js
@@ -0,0 +1,19 @@
+// Bubble Sort
+
+let theNumbers = [5, 15, 3, 8, 9, 1, 20, 7];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  console.log(bubbleSort(theNumbers));
+}
+
+function draw() {
+  background(220);
+}
+
+function bubbleSort (someArray) {
+  someArray = numbers;
+
+  return someArray;
+
+}
